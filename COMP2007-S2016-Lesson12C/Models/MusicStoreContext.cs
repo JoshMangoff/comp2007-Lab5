7c1e0c6b9b972c29b89f17842daaf212b6f2d03c
JoshMangoff <jmangoff@hotmail.com>
2016-08-04 18:03:33 -0400

    Added Shopping Cart

--- COMP2007-S2016-Lesson12C/Models/MusicStoreContext.cs
+++ COMP2007-S2016-Lesson12C/Models/MusicStoreContext.cs
@@ -15,6 +15,8 @@ namespace COMP2007_S2016_Lesson12C.Models
         public virtual DbSet<Album> Albums { get; set; }
         public virtual DbSet<Genre> Genres { get; set; }
         public virtual DbSet<Artist> Artists { get; set; }
-
+        public virtual DbSet<Cart> Carts { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
